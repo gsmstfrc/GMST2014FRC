87297ac80d44745861009fd8fe34115114d16467
John <gsmstfrc@gmail.com>
2014-01-17 14:25:59 -0500

    test commit

--- DemoBot/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ DemoBot/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -1,11 +1,11 @@
 package edu.wpi.first.wpilibj.templates;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.SimpleRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.Watchdog;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.tables.ITable;
 import edu.wpi.first.wpilibj.tables.ITableListener;
@@ -28,10 +28,11 @@ public class RobotTemplate extends SimpleRobot
     {
         joystick = new Joystick(1);
 
-        frontLeft = new Victor(4);
-        frontRight = new Victor(1);
-        backLeft = new Victor(3);
+        frontLeft = new Victor(3);
+        frontRight = new Victor(9);
+        backLeft = new Victor(4);
         backRight = new Victor(2);
+        
         watchdog = Watchdog.getInstance();
     }
 
@@ -48,9 +49,9 @@ public class RobotTemplate extends SimpleRobot
         }
     }
     //Customizable values
-    double maxXSpeed = .1;
-    double maxYSpeed = .4;
-    double maxTSpeed = .4;
+    public double maxXSpeed = 1;//.1;
+    public double maxYSpeed = 1;//.4;
+    public double maxTSpeed = .5;//.4;
     double x;
     double y;
     double t;
@@ -62,13 +63,30 @@ public class RobotTemplate extends SimpleRobot
         y = joystick.getY() * maxYSpeed;
         t = joystick.getTwist() * maxTSpeed;
 
-        frontRight.set(y + x);
-        backRight.set(y + x);
+        if(Math.abs(joystick.getX()) < .05)
+        {
+            x = 0;
+        }
+        
+        if(Math.abs(joystick.getY()) < .05)
+        {
+            y = 0;
+        }
+        
+        if(Math.abs(joystick.getTwist()) < .05)
+        {
+            t = 0;
+        }
+        
+        frontRight.set((y - t - x));
+        backRight.set((y - t + x));
 
-        backLeft.set(-y + x);
-        frontLeft.set(-y + x);
+        backLeft.set((y + t + x));
+        frontLeft.set((y + t - x));
     }
 
+    public void 
+            
     //@Override not supported
     public void autonomous()
     {
@@ -83,37 +101,87 @@ public class RobotTemplate extends SimpleRobot
 
     public class SmartDashboardUpdater implements Runnable, ITableListener
     {
-
+        
+        Preferences preferences;
         boolean saveDashboardValues = false;
         NetworkTable sd;
+        
         public SmartDashboardUpdater()
         {
         }
-        
-        public void valueChanged(ITable source, String key, Object value, boolean isNew) 
+
+        public void valueChanged(ITable source, String key, Object value, boolean isNew)
         {
-            if(key.equalsIgnoreCase("saveDashboardValues"))
+            
+            if (key.equalsIgnoreCase("saveDashboardValues"))
             {
                 Boolean update;
                 update = ((Boolean) value);
-                if(!update.booleanValue())
+                
+                if (!update.booleanValue())
+                {
                     return;
+                }
+                
                 sd.putBoolean("saveDashboardValues", false);
                 retrieveDashboardValues();
+                save();
             }
+            
         }
         
         public void retrieveDashboardValues()
         {
             //bla bla bla get values.
+            maxXSpeed = sd.getNumber("maxXSpeed", maxXSpeed);
+            maxYSpeed = sd.getNumber("maxYSpeed", maxYSpeed);
+            maxTSpeed = sd.getNumber("maxTSpeed", maxTSpeed);
+            save();
+        }
+        
+        public void sendDashboardValues()
+        {
+            sd.putNumber("maxXSpeed", maxXSpeed);
+            sd.putNumber("maxYSpeed", maxYSpeed);
+            sd.putNumber("maxTSpeed", maxTSpeed);
+        }
+        
+        public void load()
+        {
+            maxXSpeed = preferences.getDouble("maxXSpeed", maxXSpeed);
+            maxYSpeed = preferences.getDouble("maxYSpeed", maxYSpeed);
+            maxTSpeed = preferences.getDouble("maxTSpeed", maxTSpeed);
+            
+            debugingEnabled = preferences.getBoolean("debugingEnabled", debugingEnabled);
+            
+            sendDashboardValues();   
+        }
+        
+        public void save()
+        {
+            preferences.putDouble("maxXSpeed",maxXSpeed);
+            preferences.putDouble("maxYSpeed",maxYSpeed);
+            preferences.putDouble("maxTSpeed",maxTSpeed);
+            
+            preferences.putBoolean("debugingEnabled", debugingEnabled);
+            
+            preferences.save();
+        }
+        
+        //Initilize the SmartDashboard by loading the saved perferences.
+        public void init()
+        {
+            preferences = Preferences.getInstance();
+            load();
+            sd.putBoolean("saveDashboardValues", true);
         }
 
         public void run()
         {
             NetworkTable.setTeam(3318);
             sd = NetworkTable.getTable("SmartDashboard");
-            sd.addTableListener("saveDashboardValues",this, true); //True is for immedeatly notify.
-            
+            sd.addTableListener("saveDashboardValues", this, true); //True is for immedeatly notify.
+            init();
             while (true)
             {
                 //Do some code that manages updating network table stuff.
@@ -121,4 +189,5 @@ public class RobotTemplate extends SimpleRobot
 
         }
     }
+      public static boolean debugingEnabled = true;
 }
