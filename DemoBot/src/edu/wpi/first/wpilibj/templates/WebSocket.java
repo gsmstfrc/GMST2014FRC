5ba6c18a6cc762d6ea51b3a210215dfbc19f9917
John <gsmstfrc@gmail.com>
2014-04-15 21:48:47 -0400

    Working on WebSocket. Finished handshaking. Working on reading frames.

--- DemoBot/src/edu/wpi/first/wpilibj/templates/WebSocket.java
+++ DemoBot/src/edu/wpi/first/wpilibj/templates/WebSocket.java
@@ -17,13 +17,17 @@ public class WebSocket
     InputStream is;
     PrintStream os;
     String key;
+    WebServerClean.ConnectionHandler conHan;
 
-    public WebSocket(SocketConnection s, InputStream is, PrintStream os, String key)
+    public WebSocket(WebServerClean.ConnectionHandler conHan, SocketConnection s, InputStream is, PrintStream os, String key)
     {
+        this.conHan = conHan;
         this.s = s;
         this.is = is;
         this.os = os;
         this.key = key;
+        handshake();
+        run();
     }
 
     public void handshake()
@@ -32,14 +36,43 @@ public class WebSocket
         {
             String toWrite = "HTTP/1.1 101 Switching Protocols \r\n"
                     + "Connection: Upgrade\r\nUpgrade: websocket\r\n"
-                    + "Sec-WebSocket-Accept: " + SHA1.encode(new String(new String(key).getBytes("UTF8")) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11" + "\r\n"
-                    + "\r\n");
+                    + "Sec-WebSocket-Accept: " + SHA1.encode(new String(key.getBytes("UTF8")) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11") + "\r\n"
+                    + "\r\n";
             os.println(toWrite);
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
+    }
+    
+    public String readNextData()
+    {
+        try
+        {
+            byte[] header = new byte[2];
+            is.read(header);
+           
+        }
+        catch(Exception e){System.out.println("There was an error reading a frame.");}
+        
+        return "";
+    }
+
+    public void run()
+    {
+        try
+        {
+            while (true)
+            {
+                
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
     }
 
 }
