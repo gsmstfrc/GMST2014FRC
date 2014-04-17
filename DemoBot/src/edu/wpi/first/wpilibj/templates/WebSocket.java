4e997781c796af2e48bd617b3ded7ec6fc68b20a
John <gsmstfrc@gmail.com>
2014-04-16 22:48:34 -0400

    Websocket reading finished, tested, and working.

--- DemoBot/src/edu/wpi/first/wpilibj/templates/WebSocket.java
+++ DemoBot/src/edu/wpi/first/wpilibj/templates/WebSocket.java
@@ -1,6 +1,7 @@
 package edu.wpi.first.wpilibj.templates;
 
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import javax.microedition.io.SocketConnection;
 
@@ -36,27 +37,101 @@ public class WebSocket
         {
             String toWrite = "HTTP/1.1 101 Switching Protocols \r\n"
                     + "Connection: Upgrade\r\nUpgrade: websocket\r\n"
-                    + "Sec-WebSocket-Accept: " + SHA1.encode(new String(key.getBytes("UTF8")) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11") + "\r\n"
+                    + "Sec-WebSocket-Accept: " + SHA1.encode(key + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11") + "\r\n"
                     + "\r\n";
-            os.println(toWrite);
+            os.write(toWrite);
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
     }
-    
+
     public String readNextData()
     {
+        String data = "";
         try
         {
-            byte[] header = new byte[2];
-            is.read(header);
-           
+            boolean isFinished = false;
+            while (!isFinished)
+            {
+                //SETUP
+                boolean extended = false;
+                int payloadLength = 0;
+                byte[] b = new byte[2];
+                while (is.available() == 0);
+                is.read(b);
+                int maskp1 = (b[0] < 0 ? 256 + b[0] : b[0]); //convert to unsigned
+                if (maskp1 > 128)                            //if it's bigger than 128, then the first byte MUST BE 1. That means, the first bit is 1.
+                    isFinished = true;
+                int maskp2 = (b[1] < 0 ? 256 + b[1] : b[1]); //convert to unsigned.
+
+                if ((maskp2 - 128) == 126) //we subtract 128, because from client to server, it's always encrypted.
+                {
+                    byte[] b2 = new byte[2]; //16 bit integer.
+                    is.read(b2);
+                    payloadLength = ((0xFF & b2[0]) << 8) | (0xFF & b2[1]);
+                }
+                else if ((maskp2 - 128) <= 125)
+                {
+                    payloadLength = maskp2 - 128;
+                }
+                else
+                {
+                    byte[] b2 = new byte[8];
+                    is.read(b2);
+                    long value = 0;
+                    for (int i = 0; i < b2.length; i++)
+                    {
+                        value = (value << 8) + (b2[i] & 0xff);
+                    }
+                    payloadLength = (int) value;//THIS WILL ONLY WORK IF THE LENGTH OF THE PAYLOAD IS LESS THAN 2147483647 BYTES!!!!! OTHERWISE OVERFLOW!
+                }
+                byte[] key = new byte[4];
+                is.read(key);
+
+                //SETUP
+                byte[] payload = new byte[payloadLength];
+                is.read(payload);
+
+                for (int i = 0; i < payload.length; i++)
+                {
+                    payload[i] = (byte) (payload[i] ^ key[i % 4]);
+                }
+                data = new String(payload);
+                //System.out.println("" + data);
+            }
         }
-        catch(Exception e){System.out.println("There was an error reading a frame.");}
-        
-        return "";
+        catch (Exception e)
+        {
+            System.out.println("There was an error reading a frame.");
+        }
+
+        return data;
+    }
+
+    public void writeData(String toWrite)
+    {
+        int payloadLength = toWrite.length();
+        int firstByte = 129; //we are going to assume that the entire message fits into a single frame.
+        byte b1 = (byte) firstByte;
+
+        int secondByte;
+        int extendedByte;
+        if (payloadLength <= 125) //size in bytes.
+        {
+            secondByte = payloadLength;
+        }
+
+        else if (payloadLength <= 65535) //size in bytes. just under 64 kilobyte max size.
+        {
+
+        }
+        else                            //
+        {
+
+        }
+
     }
 
     public void run()
@@ -65,7 +140,15 @@ public class WebSocket
         {
             while (true)
             {
-                
+                String information = readNextData();
+                if (information.equalsIgnoreCase("true"))
+                {
+                    RobotTemplate.GoalDetected = true;
+                }
+                if (information.equalsIgnoreCase("false"))
+                {
+                    RobotTemplate.GoalDetected = false;
+                }
             }
         }
         catch (Exception e)
